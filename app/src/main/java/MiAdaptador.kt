72487ccdd58a1f1549d28c89fbcdf8487a65fd1f
alumno <ayoubbenjaddi5@gmail.com>
2025-12-05 13:44:25 +0100

    PELICULA

---
+++ app/src/main/java/MiAdaptador.kt
@@ -0,0 +1,32 @@
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.ayoub.peliculas.Pelicula
+import com.ayoub.peliculas.R
+
+class MiAdaptador(private val context: Context, private val dataSource: List<Pelicula>) : BaseAdapter() {
+    private val inflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    override fun getCount(): Int {
+        return dataSource.size
+    }
+    override fun getItem(position: Int): Pelicula {
+        return dataSource[position]
+    }
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val filaView = convertView ?: inflater.inflate(R.layout.mi_fila_personalizada, parent, false)
+        val textView: TextView = filaView.findViewById(R.id.textoFila)
+        val imageView: ImageView = filaView.findViewById(R.id.imagenFila)
+        val pais = getItem(position)
+        textView.text = pais.nombre
+        imageView.setImageResource(pais.idImagen)
+        return filaView
+    }
+}
---
+++ app/src/main/java/com/ayoub/peliculas/Pelicula.kt
@@ -0,0 +1,6 @@
+package com.ayoub.peliculas
+
+data class Pelicula (val titulo:String,val director:String,val anio:Int,val sinopsis:String,val caratula:Int){
+
+    val seleccionada: Boolean=false
+}
